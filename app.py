e0ced315f6b47b378345f962ef5b6e132cf33779
igorvoytenko <kotmarsohot@gmail.com>
2021-04-24 17:53:38 +0700

    Init

---
+++ app.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+# Получаем номер порта из переменной окружения PORT (Heroku запишет туда доступный нам порт)
+# Если в переменной PORT пусто, то по дефолту установим возьмем порт 8443
+PORT = int(os.environ.get('PORT', 8443))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
